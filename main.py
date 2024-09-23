676c4f0e319da9aeaac4bde25a84f5b4e39953ba
IuryQuintino <quintinoschoice@gmail.com>
2024-09-23 20:34:23 -0300

    PRIMEIRA AULA
    DECLARAR VARIAVÉIS

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Olá, Paulino")
+nota1=float(input("Digite sua primeira nota: "))
+nota2=float(input("Digite sua segunda nota: "))
+nota3=float(input("Digite sua terceira nota: "))
+media=(nota1+nota2+nota3)/3
+print("Sua média é: ",media)
+if media>=9:
+    print("Voçê foi Aprovado com Distinção!")
+elif media>=6:
+    print("Voçê foi Aprovado!")
+else:
+    print("Voçê foi Reprovado!")
+    
+    
+
